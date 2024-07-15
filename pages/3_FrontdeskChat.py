255a6422b41ae698d0e212d6d67a4f8e338660b5
Sujay Srivastava <62601454+sujay3srivastava@users.noreply.github.com>
2024-07-15 15:33:43 +0530

    Update 3_FrontdeskChat.py
    Random Reply bot

--- pages/3_FrontdeskChat.py
+++ pages/3_FrontdeskChat.py
@@ -1,13 +1,30 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-
+import random
+import time
 
 st.set_page_config(page_title="Frontdesk_Chat", page_icon="💬")
 st.markdown("Chat")
 st.sidebar.header("Chat")
 
 st.title("Frontdesk Chat")
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -16,17 +33,15 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-# React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
+        response = st.write_stream(response_generator())
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
